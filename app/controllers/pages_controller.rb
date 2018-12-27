e9db4552e39a5e607a4f1cc8fd9af78c79609ba8
ping mao <ping.devguru@gmail.com>
2018-12-27 17:24:12 +0800

    TA-1812271723: insert page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def about
+    @title = 'About this web page';
+    @content = 'This is content. I wanna grow as fast as possible.';
+  end
+  def new
+
+  end
+  def create
+    render plain: params[:post].inspect
   end
 end
